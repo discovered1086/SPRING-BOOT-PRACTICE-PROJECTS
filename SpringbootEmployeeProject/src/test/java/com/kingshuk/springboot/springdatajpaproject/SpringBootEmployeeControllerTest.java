10a26170d7a76509c8d8ed945c78d9c409776883
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-01-26 19:52:43 -0600

    Testing the endpoint using the rest template class

--- SpringbootEmployeeProject/src/test/java/com/kingshuk/springboot/springdatajpaproject/SpringBootEmployeeControllerTest.java
+++ SpringbootEmployeeProject/src/test/java/com/kingshuk/springboot/springdatajpaproject/SpringBootEmployeeControllerTest.java
@@ -1,21 +1,17 @@
 package com.kingshuk.springboot.springdatajpaproject;
 
+import static org.junit.Assert.*;
+
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.client.RestTemplate;
 
 import com.kingshuk.springboot.springdatajpaproject.controller.EmployeeManagementController;
 import com.kingshuk.springboot.springdatajpaproject.dto.AddressDto;
@@ -24,27 +20,21 @@ import com.kingshuk.springboot.springdatajpaproject.dto.EmployeeDto;
 import com.kingshuk.springboot.springdatajpaproject.entities.Address;
 import com.kingshuk.springboot.springdatajpaproject.entities.Department;
 import com.kingshuk.springboot.springdatajpaproject.entities.Employee;
-import com.kingshuk.springboot.springdatajpaproject.service.EmployeeManagementService;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest(classes = {EmployeeManagementController.class})
-@WebMvcTest(value=EmployeeManagementController.class)
-public class SpringbootEmployeeProjectApplicationTests {
-	
-	@Autowired
-	MockMvc mockMvc;
+@SpringBootTest(classes = EmployeeManagementController.class)
+public class SpringBootEmployeeControllerTest {
 
-	@MockBean
-	EmployeeManagementService employeeService;
-	
-	
+	@Autowired
+	private RestTemplateBuilder restTemplateBuilder;
+
 	static List<EmployeeDto> employeeDtos;
-	
+
 	static List<Employee> employees;
-	
+
 	static Employee employee;
-	
-	static{
+
+	static {
 		AddressDto address = new AddressDto();
 		address.setAddressId(9L);
 		address.setAddressLine1("771 Shady Grove Ln");
@@ -52,42 +42,35 @@ public class SpringbootEmployeeProjectApplicationTests {
 		address.setCity("Buffalo Grove");
 		address.setState("IL");
 		address.setZipCode("60089");
-		
+
 		DepartmentDto departmentDto = new DepartmentDto();
 		departmentDto.setDepartmentId(9l);
 		departmentDto.setDepartmentName("IT");
-		
+
 		EmployeeDto employeeDto = new EmployeeDto();
 		employeeDto.setEmployeeId(9l);
-		employeeDto.setFirstName("Kingshuk");
+		employeeDto.setFirstName("Amit");
 		employeeDto.setLastName("Mukherjee");
 		employeeDto.setAddress(address);
 		employeeDto.setDepartment(departmentDto);
-		
+
 		employeeDtos = Arrays.asList(employeeDto);
-		
+
 		employee = new Employee(employeeDto, new Address(address), new Department(departmentDto));
-		
+
 		employees = Arrays.asList(employee);
-		
+
 	}
-	
-	
 
 	@Test
-	public void testGetRequestForAllEmployees() throws Exception {
+	public void testSingleEmployee() {
+		RestTemplate restTemplate = restTemplateBuilder.build();
+		EmployeeDto employee2 = restTemplate.getForObject("http://localhost:8080/SpringBootEmployeeProject/employees/9"
+				, EmployeeDto.class);
 		
-		String expectedResult =  "[{\"employeeId\":1,\"firstName\":\"Kingshuk\",\"lastName\":\"Mukherjee\",\"department\":{\"departmentId\":1,\"departmentName\":\"IT\"},\"address\":{\"addressId\":1,\"addressLine1\":\"771 Shady Grove Ln\",\"addressLine2\":\"\",\"city\":\"Buffalo Grove\",\"state\":\"IL\",\"zipCode\":\"60089\"}}]";
+		//assertEquals(employee, employee2);
+		assertNotNull(employee2);
 		
-		Mockito.when(employeeService.getAllEmployees()).thenReturn(employees);
-		
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/employees")
-								.accept(MediaType.APPLICATION_JSON_VALUE);
-		
-		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-		
-		JSONAssert.assertEquals(expectedResult, result.getResponse().getContentAsString(), true);
 	}
 
 }
-
