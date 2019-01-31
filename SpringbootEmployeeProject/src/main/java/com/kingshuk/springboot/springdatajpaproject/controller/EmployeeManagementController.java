3f4a0b2f571e3847347483f5dc98b2905f7d61a8
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-01-30 23:35:39 -0600

    Logging in spring boot
    These set of changes define how to redirect logging to a file using the
    application.properties file

--- SpringbootEmployeeProject/src/main/java/com/kingshuk/springboot/springdatajpaproject/controller/EmployeeManagementController.java
+++ SpringbootEmployeeProject/src/main/java/com/kingshuk/springboot/springdatajpaproject/controller/EmployeeManagementController.java
@@ -2,6 +2,8 @@ package com.kingshuk.springboot.springdatajpaproject.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +17,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kingshuk.springboot.springdatajpaproject.dto.EmployeeDto;
-import com.kingshuk.springboot.springdatajpaproject.entities.Address;
-import com.kingshuk.springboot.springdatajpaproject.entities.Department;
 import com.kingshuk.springboot.springdatajpaproject.entities.Employee;
 import com.kingshuk.springboot.springdatajpaproject.service.EmployeeManagementService;
 
 @RestController
 public class EmployeeManagementController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeManagementController.class);
 
 	@Autowired
 	private EmployeeManagementService employeeManagementService;
@@ -41,37 +43,37 @@ public class EmployeeManagementController {
 
 	@GetMapping(path = "/employees", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public ResponseEntity<List<Employee>> getAllEmployees() {
-		
 		List<Employee> employeeList = employeeManagementService.getAllEmployees();
-		
-		if(employeeList==null || employeeList.isEmpty()) {
+
+		if (employeeList == null || employeeList.isEmpty()) {
 			return ResponseEntity.noContent().build();
 		}
-		
-		
+
 		return ResponseEntity.ok(employeeList);
 	}
 
 	@GetMapping(path = "/employees/{employeeId}", produces = { MediaType.APPLICATION_JSON_VALUE,
 			MediaType.APPLICATION_XML_VALUE })
 	public Employee getEmployeeByEmployeeId(@PathVariable("employeeId") Long empId) {
+		if (LOGGER.isInfoEnabled()) {
+			LOGGER.info(String.format("Request for employee with employee id: %s", empId));
+		}
+
 		return employeeManagementService.getEmployeeById(empId);
 	}
 
 	@PutMapping(path = "/employees", consumes = { MediaType.APPLICATION_JSON_VALUE })
 	public Employee updateEmployee(@RequestBody EmployeeDto employee) {
-		
+
 		return employeeManagementService.addOrUpdateEmployee(employee);
-		
+
 	}
-	
+
 	@PatchMapping(path = "/employees", consumes = { MediaType.APPLICATION_JSON_VALUE })
 	public void updatePartialEmployeeDto(@RequestBody EmployeeDto employeeDto) {
 		employeeManagementService.partialUpdateEmployee(employeeDto);
 	}
-	
-	
-	
+
 	@DeleteMapping(path = "/employees/{employeeId}")
 	public void deleteEmployee(@PathVariable("employeeId") Long empId) {
 		employeeManagementService.deleteEmployee(empId);
