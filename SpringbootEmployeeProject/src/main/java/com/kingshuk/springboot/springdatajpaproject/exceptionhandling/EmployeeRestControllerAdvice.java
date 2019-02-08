db46f8a36ea5c84303eaa55e4589c0c4235c281e
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-02-07 22:18:10 -0600

    Modifying the controller advice class

--- SpringbootEmployeeProject/src/main/java/com/kingshuk/springboot/springdatajpaproject/exceptionhandling/EmployeeRestControllerAdvice.java
+++ SpringbootEmployeeProject/src/main/java/com/kingshuk/springboot/springdatajpaproject/exceptionhandling/EmployeeRestControllerAdvice.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.kingshuk.springboot.springdatajpaproject.controller.EmployeeManagementController;
 import com.kingshuk.springboot.springdatajpaproject.exceptionhandling.beans.MyCustomErrorResponse;
@@ -13,7 +14,7 @@ public class EmployeeRestControllerAdvice {
 	
 	//Moving the exception handling to this class for employee controller
 	@ExceptionHandler
-	public ResponseEntity<MyCustomErrorResponse> handleException
+	public ResponseEntity<MyCustomErrorResponse> handleEmployeeNotFoundException
 	(EmployeeNotFoundException employeeNotFoundException){
 		
 		MyCustomErrorResponse customErrorResponse = new MyCustomErrorResponse();
@@ -22,6 +23,20 @@ public class EmployeeRestControllerAdvice {
 		customErrorResponse.setMessage("I'm sorry honey!! This employee does not exist...");
 		
 		return new ResponseEntity<>(customErrorResponse, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler
+	//@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "InvalidInput")
+	public ResponseEntity<MyCustomErrorResponse> handleGenericException
+	(Exception exception){
+		
+		MyCustomErrorResponse customErrorResponse = new MyCustomErrorResponse();
+		
+		customErrorResponse.setStatusCode(HttpStatus.BAD_REQUEST.value());
+		customErrorResponse.setMessage("I'm sorry honey!! You have sent some nasty shit this way!!" 
+					+exception.getMessage());
+		
+		return new ResponseEntity<>(customErrorResponse, HttpStatus.BAD_REQUEST);
 	}
 
 }
