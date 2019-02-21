4f9c05f8424e165134be15c9f5bef51ee14cfbd8
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-02-21 00:35:36 -0600

    Adding updates to the category repository

--- MultiModuleSpringBootProject/src/main/java/com/kingshuk/springboot/multimodulespringboot/model/repos/CategoryRepository.java
+++ MultiModuleSpringBootProject/src/main/java/com/kingshuk/springboot/multimodulespringboot/model/repos/CategoryRepository.java
@@ -1,9 +1,11 @@
 package com.kingshuk.springboot.multimodulespringboot.model.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.kingshuk.springboot.multimodulespringboot.model.entities.Category;
 
+@RepositoryRestResource(path="categories")
 public interface CategoryRepository extends JpaRepository<Category, String> {
 
 }
