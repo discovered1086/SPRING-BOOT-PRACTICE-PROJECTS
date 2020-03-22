5d47ad47038cbb45befdb54cfe7f392973400efe
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-03-22 14:21:13 -0500

    Some additional changes to the mock mvc test

--- SpringbootEmployeeProject/src/test/java/com/kingshuk/springboot/springdatajpaproject/SpringbootEmployeeProjectApplicationTests.java
+++ SpringbootEmployeeProject/src/test/java/com/kingshuk/springboot/springdatajpaproject/SpringbootEmployeeProjectApplicationTests.java
@@ -1,10 +1,13 @@
 package com.kingshuk.springboot.springdatajpaproject;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -13,12 +16,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.kingshuk.springboot.springdatajpaproject.config.MyCustomConfiguration;
 import com.kingshuk.springboot.springdatajpaproject.controller.EmployeeManagementController;
 import com.kingshuk.springboot.springdatajpaproject.dto.AddressDto;
 import com.kingshuk.springboot.springdatajpaproject.dto.DepartmentDto;
@@ -29,29 +34,31 @@ import com.kingshuk.springboot.springdatajpaproject.entities.Employee;
 import com.kingshuk.springboot.springdatajpaproject.service.EmployeeManagementService;
 
 /**
- * What this class is doing here is performing unit testing, not end to end 
- * integration test and that's exactly why we have mocked out the employee management service class
+ * What this class is doing here is performing unit testing, not end to end
+ * integration test and that's exactly why we have mocked out the employee
+ * management service class
+ * 
  * @author kingshuksmacbookpro
  *
  */
 @RunWith(SpringRunner.class)
 //@SpringBootTest(classes = {EmployeeManagementController.class})
-@WebMvcTest(value=EmployeeManagementController.class)
+@WebMvcTest(value = EmployeeManagementController.class)
+@ContextConfiguration(classes = MyCustomConfiguration.class)
 public class SpringbootEmployeeProjectApplicationTests {
-	
+
 	@Autowired
 	MockMvc mockMvc;
 
 	@MockBean
 	EmployeeManagementService employeeService;
-	
-	
+
 	List<EmployeeDto> employeeDtos;
-	
+
 	List<Employee> employees;
-	
+
 	Employee employee;
-	
+
 	@PostConstruct
 	public void loadDataForTesting() {
 		AddressDto address = new AddressDto();
@@ -61,41 +68,41 @@ public class SpringbootEmployeeProjectApplicationTests {
 		address.setCity("Buffalo Grove");
 		address.setState("IL");
 		address.setZipCode("60089");
-		
+
 		DepartmentDto departmentDto = new DepartmentDto();
 		departmentDto.setDepartmentId(9l);
 		departmentDto.setDepartmentName("IT");
-		
+
 		EmployeeDto employeeDto = new EmployeeDto();
 		employeeDto.setEmployeeId(9l);
 		employeeDto.setFirstName("Kingshuk");
 		employeeDto.setLastName("Mukherjee");
 		employeeDto.setAddress(address);
 		employeeDto.setDepartment(departmentDto);
-		
+
 		employeeDtos = Arrays.asList(employeeDto);
-		
+
 		employee = new Employee(employeeDto, new Address(address), new Department(departmentDto));
-		
+
 		employees = Arrays.asList(employee);
 	}
-	
-	
 
 	@Test
 	public void testGetRequestForAllEmployees() throws Exception {
-		
-		String expectedResult =  "[{\"employeeId\":1,\"firstName\":\"Kingshuk\",\"lastName\":\"Mukherjee\",\"department\":{\"departmentId\":1,\"departmentName\":\"IT\"},\"address\":{\"addressId\":1,\"addressLine1\":\"771 Shady Grove Ln\",\"addressLine2\":\"\",\"city\":\"Buffalo Grove\",\"state\":\"IL\",\"zipCode\":\"60089\"}}]";
-		
+		File jsonFile = new File(getClass().getClassLoader().getResource("jsonFiles/employee.json").getFile());
+
+		String expectedResult = FileUtils.readFileToString(jsonFile, StandardCharsets.UTF_8);
+
 		Mockito.when(employeeService.getAllEmployees()).thenReturn(employees);
-		
+
 		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/employees")
-								.accept(MediaType.APPLICATION_JSON_VALUE);
-		
+				.accept(MediaType.APPLICATION_JSON_VALUE);
+
 		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-		
-		JSONAssert.assertEquals(expectedResult, result.getResponse().getContentAsString(), true);
+
+		String actualResult = result.getResponse().getContentAsString();
+
+		JSONAssert.assertEquals(expectedResult, actualResult, true);
 	}
 
 }
-
