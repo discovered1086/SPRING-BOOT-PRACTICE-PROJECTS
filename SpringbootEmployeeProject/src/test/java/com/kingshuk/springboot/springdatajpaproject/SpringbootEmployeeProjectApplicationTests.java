a2cda17a12b060744d1de39ecda66c25d2ae2af4
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-04-10 17:49:44 -0500

    Attempts to fix the testing issues

--- SpringbootEmployeeProject/src/test/java/com/kingshuk/springboot/springdatajpaproject/SpringbootEmployeeProjectApplicationTests.java
+++ SpringbootEmployeeProject/src/test/java/com/kingshuk/springboot/springdatajpaproject/SpringbootEmployeeProjectApplicationTests.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -40,6 +41,7 @@ import com.kingshuk.springboot.springdatajpaproject.service.EmployeeManagementSe
  */
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 @WebMvcTest(controllers = EmployeeManagementController.class)
 @ContextConfiguration(classes = MyCustomConfiguration.class)
 public class SpringbootEmployeeProjectApplicationTests {
