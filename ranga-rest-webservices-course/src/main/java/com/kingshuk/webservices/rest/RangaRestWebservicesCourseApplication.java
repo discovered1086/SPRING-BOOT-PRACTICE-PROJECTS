1329df770ae120d0ac36f3d3976de0607539584b
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-10-24 22:09:39 -0500

    Introduction to swagger documentation on this API

--- ranga-rest-webservices-course/src/main/java/com/kingshuk/webservices/rest/RangaRestWebservicesCourseApplication.java
+++ ranga-rest-webservices-course/src/main/java/com/kingshuk/webservices/rest/RangaRestWebservicesCourseApplication.java
@@ -2,12 +2,23 @@ package com.kingshuk.webservices.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RangaRestWebservicesCourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RangaRestWebservicesCourseApplication.class, args);
+	}
+	
+	@Bean
+	public Docket apiDocumentation() {
+		return new Docket(DocumentationType.SWAGGER_2);
 	}
 	
 //	@Bean
